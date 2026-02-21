296ce17a109747e1933c41342b8946180b3bdca6
PrasadBabuPhad <pphad4925@gmail.com>
2026-02-21 10:15:54 +0530

    Counter code

--- Day6/script.js
+++ Day6/script.js
@@ -1,18 +1,29 @@
-import React from "react"
+import React, {useState} from "react"
 import ReactDOM from 'react-dom/client'
 
 function Counter(){
-    let count=0;
+    let [count, setCount] = useState(0);
 
-    function increment(){
-        
+    function incrementnumber(){
+        count+=1;
+        setCount(count);
+        //document.querySelector('h1').innerText=`Count is ${count}`;
+        //document.querySelector('button').innerText=`Increment is ${count}`;
     }
+
+    function decrementnumber(){
+        count-=1;
+        setCount(count);
+        //document.querySelector('h1').innerText=`Count is ${count}`;
+        //document.querySelector('button').innerText=`Decrement is ${count}`;
+    }
+
 
     return (
         <div className="first">
             <h1>Count is:{count}</h1>
-            <button onClick={incrementnumber}>Increment</button>
-            <button>Decrement</button>
+            <button onClick={incrementnumber}>Increment {count}</button>
+            <button onClick={decrementnumber}>Decrement {count}</button>
         </div>
         
     )
