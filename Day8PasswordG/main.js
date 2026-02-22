587d4bf8680451727b7d1543c2711857f749cc9b
PrasadBabuPhad <pphad4925@gmail.com>
2026-02-22 19:16:37 +0530

    Password changer added

---
+++ Day8PasswordG/main.js
@@ -0,0 +1,14 @@
+import React,{useEffect,useState} from "react";
+import ReactDOM from "react-dom/client"
+
+function PasswordGenerator(){
+    return(
+        <>
+        <div>
+        </div>
+        </>
+    )
+}
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<PasswordGenerator/>);
