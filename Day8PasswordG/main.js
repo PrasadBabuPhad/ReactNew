49cf7081ff2c942eabd936b81ea91235ca8d6370
PrasadBabuPhad <pphad4925@gmail.com>
2026-02-23 18:15:30 +0530

    structure created

--- Day8PasswordG/main.js
+++ Day8PasswordG/main.js
@@ -1,10 +1,27 @@
 import React,{useEffect,useState} from "react";
 import ReactDOM from "react-dom/client"
 
+
+
 function PasswordGenerator(){
+
+    const [Password, setPassword]= useState("DSFSFRER");
+    const [length,setLength]=useState(10);
+
+
     return(
         <>
         <div>
+            <h1>Password is:{Password}</h1>
+
+            <input type="range" min={5} max={15}></input>
+            <label>Length is:{length}</label>
+
+            <input type="checkbox"></input>
+            <label>Number</label>
+
+            <input type="checkbox"></input>
+            <label>Character</label>
         </div>
         </>
     )
