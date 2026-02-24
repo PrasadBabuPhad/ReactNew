ba6e0c678fa5db05b276a58394c31b4157abc95f
PrasadBabuPhad <pphad4925@gmail.com>
2026-02-24 15:39:18 +0530

    onchange added

--- Day8PasswordG/main.js
+++ Day8PasswordG/main.js
@@ -7,20 +7,22 @@ function PasswordGenerator(){
 
     const [Password, setPassword]= useState("DSFSFRER");
     const [length,setLength]=useState(10);
+    const [numberChanged,setnumberChanged]= useState(false);
+    const[charChanged,setcharChanged] =useState(false);
 
 
     return(
         <>
-        <div>
-            <h1>Password is:{Password}</h1>
+        <div className="second">
+            <h1>{Password}</h1>
 
-            <input type="range" min={5} max={15}></input>
-            <label>Length is:{length}</label>
+            <input type="range" min={5} max={15} value={length} onChange={(e)=>setLength(e.target.value)}></input>
+            <label>Length is:({length})</label>
 
-            <input type="checkbox"></input>
+            <input type="checkbox" defaultChecked={numberChanged} onChange={()=>setnumberChanged(!numberChanged)}></input>
             <label>Number</label>
 
-            <input type="checkbox"></input>
+            <input type="checkbox" defaultChecked={charChanged} onChange={()=>setcharChanged(!charChanged)}></input>
             <label>Character</label>
         </div>
         </>
