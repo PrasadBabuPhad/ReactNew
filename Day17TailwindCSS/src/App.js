73a9dcf8838ee6737c1d3ac0774cfdcac6abeb8b
PrasadBabuPhad <pphad4925@gmail.com>
2026-04-03 17:01:29 +0530

    basics added

---
+++ Day17TailwindCSS/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+function App(){
+    return(
+        <h1 className="bg-gray-400">Hello Coder Army Bachoo</h1>
+    )
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
